3251fee1a5d6c29e390d8c83ce39d3b002d3ddb4
sagie gur ari <sagiegurari@gmail.com>
2018-03-04 10:04:12 +0000

    pwa cache

--- service-worker.js
+++ service-worker.js
@@ -3,8 +3,20 @@
 
     const CACHE_VERSION = 1;
     const CURRENT_CACHES = {
+        assets: 'assets-v' + CACHE_VERSION,
         data: 'data-v' + CACHE_VERSION
     };
+    const CACHED_EXTENSIONS = [
+        '.png',
+        '.css',
+        '.html',
+        'app.js',
+        '.woff',
+        '.woff2',
+        '.map',
+        'manifest.json',
+        '.githubusercontent.com/u/'
+    ];
 
     const app = (function () {
         const appModule = {
@@ -38,13 +50,60 @@
     });
 
     self.addEventListener('fetch', function (event) {
-        if (event.request.url.indexOf('api.github.com/users/sagiegurari') !== -1) {
+        const request = event.request;
+        const url = request.url;
+        let cacheSkipped = true;
+
+        if (url.indexOf('api.github.com/users/sagiegurari') !== -1) {
+            cacheSkipped = false;
+
             event.respondWith(caches.open(CURRENT_CACHES.data).then(function (cache) {
-                return fetch(event.request).then(function (response) {
-                    cache.put(event.request.url, response.clone());
+                return fetch(request).then(function (response) {
+                    cache.put(url, response.clone());
+
+                    console.log('[service worker] cached data.');
+
                     return response;
                 });
             }));
+        } else {
+            for (let index = 0; index < CACHED_EXTENSIONS.length; index++) {
+                const extension = CACHED_EXTENSIONS[index];
+                if (url.indexOf(extension) !== -1) {
+                    cacheSkipped = false;
+
+                    let responded = false;
+                    const fetchAndCache = function () {
+                        if (!responded) {
+                            responded = true;
+
+                            event.respondWith(caches.open(CURRENT_CACHES.assets).then(function (cache) {
+                                return fetch(request).then(function (response) {
+                                    cache.put(url, response.clone());
+
+                                    console.log('[service worker] cached asset: ', url);
+
+                                    return response;
+                                });
+                            }));
+                        }
+                    };
+
+                    caches.match(url).then(function (response) {
+                        if (response) {
+                            return response;
+                        } else {
+                            return fetchAndCache();
+                        }
+                    }).catch(fetchAndCache);
+
+                    break;
+                }
+            }
+        }
+
+        if (cacheSkipped) {
+            console.warn('[service worker] skipped cache for url: ', url);
         }
     });
 }());
